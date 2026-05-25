5f5afa6edc3bed1e98f89cbe0386eb073b4b7e95
irwansyah <igirwansyah47@gmail.com>
2026-05-25 23:28:34 +0800

    feat: implementasi fungsi dan form tambah data Movie

---
+++ resources/views/movies/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Tambah Movie</h1>
+
+    <form action="{{ route('movies.store') }}" method="POST">
+
+        @csrf
+
+        <input type="text" name="title" class="form-control mb-2" placeholder="Judul">
+
+        <input type="text" name="genre" class="form-control mb-2" placeholder="Genre">
+
+        <input type="number" name="year" class="form-control mb-2" placeholder="Tahun">
+
+        <input type="text" name="director" class="form-control mb-2" placeholder="Director">
+
+        <input type="number" name="duration" class="form-control mb-2" placeholder="Durasi">
+
+        <button class="btn btn-success">
+
+            Simpan
+
+        </button>
+
+    </form>
+@endsection
