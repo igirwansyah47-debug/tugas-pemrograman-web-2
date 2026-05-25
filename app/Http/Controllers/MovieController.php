b0bb88cd5c1a486d1aba7d35ae2a3c68aa7d8c1f
irwansyah <igirwansyah47@gmail.com>
2026-05-25 23:27:15 +0800

    feat: implementasi fitur read untuk Movie

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $movies = Movie::all();
+
+        return view('movies.index', compact('movies'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('movies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Movie::create($request->all());
+
+        return redirect()->route('movies.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Movie $movie)
+    {
+        return view('movies.edit', compact('movie'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Movie $movie)
+    {
+        $movie->update($request->all());
+
+        return redirect()->route('movies.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Movie $movie)
+    {
+        $movie->delete();
+
+        return redirect()->route('movies.index');
+    }
 }
---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Data Movie</h1>
+
+    <a href="{{ route('movies.create') }}" class="btn btn-primary mb-3">
+
+        Tambah Movie
+    </a>
+
+    <table class="table table-bordered">
+
+        <tr>
+            <th>Judul</th>
+            <th>Genre</th>
+            <th>Tahun</th>
+            <th>Director</th>
+            <th>Durasi</th>
+            <th>Aksi</th>
+        </tr>
+
+        @foreach ($movies as $movie)
+            <tr>
+
+                <td>{{ $movie->title }}</td>
+                <td>{{ $movie->genre }}</td>
+                <td>{{ $movie->year }}</td>
+                <td>{{ $movie->director }}</td>
+                <td>{{ $movie->duration }}</td>
+
+                <td>
+
+                    <a href="{{ route('movies.edit', $movie->id) }}" class="btn btn-warning btn-sm">
+
+                        Edit
+
+                    </a>
+
+                    <form action="{{ route('movies.destroy', $movie->id) }}" method="POST" style="display:inline;">
+
+                        @csrf
+                        @method('DELETE')
+
+                        <button class="btn btn-danger btn-sm">
+
+                            Delete
+
+                        </button>
+
+                    </form>
+
+                </td>
+
+            </tr>
+        @endforeach
+
+    </table>
+@endsection
